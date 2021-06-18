0d989d4ef63c69316f94f8e58083ffd0a56a88ec
zyb123nya <1846172115@qq.com>
2021-06-18 10:26:56 +0800

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Product, products } from '../products';
+import { products } from '../products';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-  product: Product | undefined;
+  product: any;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
